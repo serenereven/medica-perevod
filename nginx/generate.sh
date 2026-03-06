4a619fa2e0b18966fe452db77dfee1e0241b957d
serenere <s.panin.center@gmail.com>
2026-03-06 16:37:29 +0500

    nginx fix

--- nginx/generate.sh
+++ nginx/generate.sh
@@ -4,14 +4,16 @@ set -e
 CERT="/etc/letsencrypt/live/${LETSENCRYPT_DOMAIN}/fullchain.pem"
 KEY="/etc/letsencrypt/live/${LETSENCRYPT_DOMAIN}/privkey.pem"
 
+rm -f /etc/nginx/conf.d/*.conf
+
 if [ -f "$CERT" ] && [ -f "$KEY" ]; then
   echo "nginx: using HTTPS config"
-  envsubst '$$NGINX_SERVER_NAME $$LETSENCRYPT_DOMAIN' \
-    < /etc/nginx/templates/https.conf.template \
+  envsubst '$NGINX_SERVER_NAME $LETSENCRYPT_DOMAIN' \
+    < /opt/nginx-templates/https.conf.template \
     > /etc/nginx/conf.d/app.conf
 else
   echo "nginx: using HTTP config (no cert yet)"
-  envsubst '$$NGINX_SERVER_NAME' \
-    < /etc/nginx/templates/http.conf.template \
+  envsubst '$NGINX_SERVER_NAME' \
+    < /opt/nginx-templates/http.conf.template \
     > /etc/nginx/conf.d/app.conf
 fi
