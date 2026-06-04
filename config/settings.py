3a8a87aaee85be5335ba26b5e1516ba343b39a88
serenere <s.panin.center@gmail.com>
2026-06-04 18:23:47 +0500

    [customer edits] documents template fix 2

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "core.api.pagination.StandardResultsSetPagination",  # было rest_framework.pagination.PageNumberPagination
     "PAGE_SIZE": 20,
 }
 
