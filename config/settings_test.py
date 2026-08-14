3f6b1eadbd7506b5730522449aefc02155886a42
serenere <s.panin.center@gmail.com>
2026-08-14 12:40:58 +0500

    SQLite for settings test

--- config/settings_test.py
+++ config/settings_test.py
@@ -1,25 +1,23 @@
 from config.settings import *
 
-DEBUG = True
-SECRET_KEY = "test-secret-key-for-testing-only"
+DEBUG = False
+SECRET_KEY = 'test-secret-key-for-testing-only'
 
+# SQLite для тестов
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
     }
 }
 
-PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-
+PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
 
 # Отключаем миграции для ускорения тестов
 class DisableMigrations:
     def __contains__(self, item):
         return True
-
     def __getitem__(self, item):
         return None
-
 
 MIGRATION_MODULES = DisableMigrations()
